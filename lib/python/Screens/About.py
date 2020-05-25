0cdc1b1f4638ae83602a142176f85a8bf107416f
Redouane <redouane343@yahoo.com>
2020-05-25 19:47:48 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -30,6 +30,7 @@ class About(Screen):
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
+		AboutText += _("Developer Name:\tRedouane ") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
